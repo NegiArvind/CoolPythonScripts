76c84a4f56413f2a74d7ca0af4b9f4332e89e151
arvind <negiarvind229@gmail.com>
2018-09-08 20:07:46 +0530

    initial commit

---
+++ youTubeVideoDownload.py
@@ -0,0 +1,66 @@
+from pytube import YouTube
+from datetime import datetime
+
+def completionMessage(stream, fileHandle):
+    print("Video Downloaded")
+
+
+def downloadVideoWithSound(stringUrl,path):
+    # youTubeVariable = YouTube("https://www.youtube.com/watch?v=aNEnzS_um9U")
+
+    print("Fetching Data...")
+
+    try:
+
+        youTubeVariable = YouTube(stringUrl)
+        # video=video.get("mp4","480")
+        myTitle = youTubeVariable.title
+        print(myTitle)  # title of you tube video
+        # print(youTubeVariable.thumbnail_url)  # png image used on you tube video
+        # print(youTubeVariable.streams.all())  # it will return all the streams available for this video
+        stream = youTubeVariable.streams.first()  # getting first stream
+        # print("first stream", stream)
+
+        # stream.download('/home/arvind/Downloads/Video',myTitle+" "+datetime.now().ctime())  # downloading video and saved into specific folder
+
+        print("Downloading...")
+        stream.download(path,myTitle + " " + datetime.now().ctime())  # downloading video and saved into specific folder
+        # second argument is the filename or videoname
+        youTubeVariable.register_on_complete_callback(
+            completionMessage(stream, None))  # Whenever video will be completed
+        # then this method will invoke
+
+        # some of the stream contains only audio or only video .We can also download them
+
+    except:
+        print("Please enter valid url")
+
+
+def downloadOnlyAudio(url,path):
+    print("Fetching Data...")
+
+    try:
+        youTubeVariable = YouTube(url)
+        # Below code is used to download only audio
+
+        myTitle = youTubeVariable.title
+        print(myTitle)
+        stream1 = youTubeVariable.streams.filter(only_audio=True).first()
+        # print(stream1)
+        # stream1.download('/home/arvind/Downloads/Video')  # downloading only audio of you tube video and
+        # #  then save into specific folder
+        #
+        print("Downloading...")
+        stream1.download(path, myTitle + " " + datetime.now().ctime())  # downloading only audio of you tube video and
+        #  then save into specific folder
+        youTubeVariable.register_on_complete_callback(completionMessage(stream1, None))
+
+    except:
+        print("Please enter a valid url")
+
+
+url=str(input("Enter the you tube url\n"))
+path=input("Enter the path where you want to save this video\n")
+downloadVideoWithSound(url,path)
+# downloadOnlyAudio(url,path)
+
